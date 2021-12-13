421475ac12f5ea4f6db9f86cac5e64c919b832da
Mitch Ross <mitchross09@gmail.com>
2021-12-13 11:35:33 -0500

    fix search results

--- packages/tvbc_repository/lib/src/themoviedb_api/models/tv/tv_search_result.dart
+++ packages/tvbc_repository/lib/src/themoviedb_api/models/tv/tv_search_result.dart
@@ -6,100 +6,114 @@ import 'dart:convert';
 
 import '../models.dart';
 
-TvSearchResults tvSearchResultsDartFromJson(String str) => TvSearchResults.fromJson(json.decode(str));
-
-String tvSearchResultsDartToJson(TvSearchResults data) => json.encode(data.toJson());
+TvSearchResults tvSearchResultsFromJson(String str) =>
+    TvSearchResults.fromJson(json.decode(str));
 
 class TvSearchResults {
-    TvSearchResults({
-        this.page,
-        this.results,
-        this.totalPages,
-        this.totalResults,
-    });
+  TvSearchResults({
+    this.page,
+    this.results,
+    this.totalPages,
+    this.totalResults,
+  });
 
-    int? page;
-    List<TVResult>? results;
-    int? totalPages;
-    int? totalResults;
+  int? page;
+  List<Result>? results;
+  int? totalPages;
+  int? totalResults;
 
-    factory TvSearchResults.fromJson(Map<String, dynamic> json) => TvSearchResults(
-        page: json["page"],
-        results: List<TVResult>.from(json["results"].map((x) => TVResult.fromJson(x))),
-        totalPages: json["total_pages"],
-        totalResults: json["total_results"],
-    );
-
-    Map<String, dynamic> toJson() => {
-        "page": page,
-        "results": List<dynamic>.from(results!.map((x) => x.toJson())),
-        "total_pages": totalPages,
-        "total_results": totalResults,
-    };
+  factory TvSearchResults.fromJson(Map<String, dynamic> json) =>
+      TvSearchResults(
+        page: json["page"] == null ? null : json["page"],
+        results: json["results"] == null
+            ? null
+            : List<Result>.from(json["results"].map((x) => Result.fromJson(x))),
+        totalPages: json["total_pages"] == null ? null : json["total_pages"],
+        totalResults:
+            json["total_results"] == null ? null : json["total_results"],
+      );
 }
 
-class TVResult {
-    TVResult({
-        this.backdropPath,
-        this.firstAirDate,
-        this.genreIds,
-        this.id,
-        this.name,
-        this.originCountry,
-        this.originalLanguage,
-        this.originalName,
-        this.overview,
-        this.popularity,
-        this.posterPath,
-        this.voteAverage,
-        this.voteCount,
-    });
+class Result {
+  Result({
+    this.backdropPath,
+    this.firstAirDate,
+    this.genreIds,
+    this.id,
+    this.name,
+    this.originCountry,
+    this.originalLanguage,
+    this.originalName,
+    this.overview,
+    this.popularity,
+    this.posterPath,
+    this.voteAverage,
+    this.voteCount,
+  });
 
-    String? backdropPath;
-    DateTime? firstAirDate;
-    List<int>? genreIds;
-    int? id;
-    String? name;
-    List<String>? originCountry;
-    OriginalLanguage? originalLanguage;
-    String? originalName;
-    String? overview;
-    double? popularity;
-    String? posterPath;
-    double? voteAverage;
-    int? voteCount;
+  String? backdropPath;
+  DateTime? firstAirDate;
+  List<int>? genreIds;
+  int? id;
+  String? name;
+  List<String>? originCountry;
+  OriginalLanguage? originalLanguage;
+  String? originalName;
+  String? overview;
+  double? popularity;
+  String? posterPath;
+  double? voteAverage;
+  int? voteCount;
 
-    factory TVResult.fromJson(Map<String, dynamic> json) => TVResult(
-        backdropPath: json["backdrop_path"] == null ? null : json["backdrop_path"],
-        firstAirDate: DateTime.tryParse(json["first_air_date"]),
-        genreIds: List<int>.from(json["genre_ids"].map((x) => x)),
-        id: json["id"],
-        name: json["name"],
-        originCountry: List<String>.from(json["origin_country"].map((x) => x)),
-        originalLanguage: originalLanguageValues.map[json["original_language"]],
-        originalName: json["original_name"],
-        overview: json["overview"],
-        popularity: json["popularity"].toDouble(),
+  factory Result.fromJson(Map<String, dynamic> json) => Result(
+        backdropPath:
+            json["backdrop_path"] == null ? null : json["backdrop_path"],
+        firstAirDate: json["first_air_date"] == null
+            ? null
+            : DateTime.parse(json["first_air_date"]),
+        genreIds: json["genre_ids"] == null
+            ? null
+            : List<int>.from(json["genre_ids"].map((x) => x)),
+        id: json["id"] == null ? null : json["id"],
+        name: json["name"] == null ? null : json["name"],
+        originCountry: json["origin_country"] == null
+            ? null
+            : List<String>.from(json["origin_country"].map((x) => x)),
+        originalLanguage: json["original_language"] == null
+            ? null
+            : originalLanguageValues.map![json["original_language"]],
+        originalName:
+            json["original_name"] == null ? null : json["original_name"],
+        overview: json["overview"] == null ? null : json["overview"],
+        popularity:
+            json["popularity"] == null ? null : json["popularity"].toDouble(),
         posterPath: json["poster_path"] == null ? null : json["poster_path"],
-        voteAverage: json["vote_average"].toDouble(),
-        voteCount: json["vote_count"],
-    );
-
-    Map<String, dynamic> toJson() => {
-        "backdrop_path": backdropPath == null ? null : backdropPath,
-        "first_air_date": "${firstAirDate!.year.toString().padLeft(4, '0')}-${firstAirDate!.month.toString().padLeft(2, '0')}-${firstAirDate!.day.toString().padLeft(2, '0')}",
-        "genre_ids": List<dynamic>.from(genreIds!.map((x) => x)),
-        "id": id,
-        "name": name,
-        "origin_country": List<dynamic>.from(originCountry!.map((x) => x)),
-        "original_language": originalLanguageValues.reverse![originalLanguage!],
-        "original_name": originalName,
-        "overview": overview,
-        "popularity": popularity,
-        "poster_path": posterPath == null ? null : posterPath,
-        "vote_average": voteAverage,
-        "vote_count": voteCount,
-    };
+        voteAverage: json["vote_average"] == null
+            ? null
+            : json["vote_average"].toDouble(),
+        voteCount: json["vote_count"] == null ? null : json["vote_count"],
+      );
 }
+
+// enum OriginalLanguage { EN, JA }
+
+// final originalLanguageValues = EnumValues({
+//     "en": OriginalLanguage.EN,
+//     "ja": OriginalLanguage.JA
+// });
+
+// class EnumValues<T> {
+//     Map<String, T>? map;
+//     Map<T, String>? reverseMap;
+
+//     EnumValues(this.map);
+
+//     Map<T, String>? get reverse {
+//         if (reverseMap == null) {
+//             reverseMap = map?.map((k, v) => new MapEntry(v, k));
+//         }
+//         return reverseMap;
+//     }
+// }
 
 
